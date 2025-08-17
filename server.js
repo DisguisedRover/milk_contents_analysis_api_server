9cba9001b1b2cfc11a1e11f4f5def5f2b2f1da96
NirajaGaha <133249442+DisguisedRover@users.noreply.github.com>
2025-08-17 13:54:22 +0545

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const db = require('./lib/models/db');
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.json());
+
+app.use('/productDetail/', require('./lib/routes/productDetailRoutes'));
+app.use('/product/', require('./lib/routes/productRoutes'));
+app.use('/auth/', require('./lib/routes/authRoutes'));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
