2eeda96bfe537ca7e6ccb89351d96b0cc1858f6c
NirajaGaha <nirajan.ngn99@gmail.com>
2025-11-02 14:43:12 +0545

    routes added for for said modules

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+
+app.use('/units/', require('./lib/routes/uintsRoutes'));
+app.use('/stockTransfers/', require('./lib/routes/stockTransferRoutes'));
+app.use('/srockLevels/', require('./lib/routes/stockLevelRoutes'));
+app.use('/salesOrders/', require('./lib/routes/salesOrderRoutes'));
+app.use('/purchaseOrders/', require('./lib/routes/purchaseOrderRoutes'));
 app.use('/customers/', require('./lib/routes/customersRoutes'));
 app.use('/categories/', require('./lib/routes/categoriesRoutes'));
 app.use('/stockTransactions/', require('./lib/routes/stockTransactionRoutes'));
